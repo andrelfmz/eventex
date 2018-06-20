2cf891e3303e1913885d18ee98f9ad87c48456d8
andrelfmz <andrelfm@gmail.com>
2018-06-20 15:56:59 -0300

    Refatora geral

---
+++ eventex/subscriptions/tests/test_mail_subscribe.py
@@ -0,0 +1,36 @@
+from django.core import mail
+from django.test import TestCase
+
+
+class SubscribePostValid(TestCase):
+
+    def setUp(self):
+        data = dict(name='André Menezes', cpf='12345678901', email='andrelfm@gmail.com', phone='34-98801-0797')
+        self.resp = self.client.post('/inscricao/', data)
+        self.email = mail.outbox[0]
+
+    def test_subscription_email_subject(self):
+        expect = 'Confirmação de inscrição'
+
+        self.assertEqual(expect, self.email.subject)
+
+    def test_subscription_email_from(self):
+        expect = 'contato@eventex.com.br'
+
+        self.assertEqual(expect, self.email.from_email)
+
+    def test_subscription_email_to(self):
+        expect = ['contato@eventex.com.br', 'andrelfm@gmail.com']
+
+        self.assertEqual(expect, self.email.to)
+
+    def test_subscription_email_body(self):
+        contents = [
+            'André Menezes',
+            '12345678901',
+            'andrelfm@gmail.com',
+            '34-98801-0797',
+        ]
+        for content in contents:
+            with self.subTest():
+                self.assertIn(content, self.email.body)
