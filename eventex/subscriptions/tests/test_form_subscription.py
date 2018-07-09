53f76889f07052b9951b5c20fae35daf24da4263
andrelfmz <andrelfm@gmail.com>
2018-07-09 16:22:08 -0300

    Email ou telefone opcionais

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -29,6 +29,20 @@ class SubscriptionFormTest(TestCase):
         form = self.make_validated_form(name='ANDRÉ menezes')
         self.assertEqual('André Menezes', form.cleaned_data['name'])
 
+    def test_email_is_optional(self):
+        """"Email is optional"""
+        form = self.make_validated_form(email='')
+        self.assertFalse(form.errors)
+
+    def test_phone_is_optional(self):
+        """"Phone is optional"""
+        form = self.make_validated_form(phone='')
+        self.assertFalse(form.errors)
+
+    def test_must_inform_email_or_phone(self):
+        """"Email and Phone are optional, but one must be informed."""
+        form = self.make_validated_form(email='', phone='')
+        self.assertListEqual(['__all__'], list(form.errors))
 
     def assertFormErrorCode(self, form, field, code):
         errors = form.errors.as_data()
@@ -36,14 +50,13 @@ class SubscriptionFormTest(TestCase):
         exception = errors_list[0]
         self.assertEqual(code, exception.code)
 
-
     def assertFormErrorMessage(self, form, field, msg):
         errors = form.errors
         errors_list = errors[field]
         self.assertListEqual([msg], errors_list)
 
     def make_validated_form(self, **kwargs):
-        valid = dict(name='André Menezes', cpf='1234567891',
+        valid = dict(name='André Menezes', cpf='12345678901',
                     email='andrelfm@gmail.com', phone='34-98801-0797')
         data = dict(valid, **kwargs)
         form = SubscriptionForm(data)
