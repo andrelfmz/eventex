464afc514eeeae0a821536ce55a979ef4f221408
andrelfmz <andrelfm@gmail.com>
2018-07-10 11:48:53 -0300

    Simplifica com ModelForm

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 from hashids import Hashids
+
+from eventex.subscriptions.validators import validate_cpf
+
 hashids = Hashids(salt='Welcome to the Django', min_length=11)
 
 
 
 class Subscription(models.Model):
     name = models.CharField('nome', max_length=100)
-    cpf = models.CharField('CPF', max_length=11)
-    email = models.EmailField('e-mail')
-    phone = models.CharField('telefone', max_length=20)
+    cpf = models.CharField('CPF', max_length=11, validators=[validate_cpf])
+    email = models.EmailField('e-mail', blank=True)
+    phone = models.CharField('telefone', max_length=20, blank=True)
     created_at = models.DateTimeField('criado em', auto_now=True)
     hashid = models.CharField(max_length=11, primary_key=True)
     paid = models.BooleanField('pago', default=False)
